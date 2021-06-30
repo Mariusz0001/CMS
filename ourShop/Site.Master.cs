267efe1d1f1000dcfb3bd16e775850e0e92529c0
Mariusz P <mariusz.pawlus95@gmail.com>
2021-06-30 19:11:42 +0200

    sql done

--- ourShop/Site.Master.cs
+++ ourShop/Site.Master.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace ourShop
@@ -14,7 +15,31 @@ namespace ourShop
         protected void Page_Load(object sender, EventArgs e)
         {
             FillLoginCard();
+            CreateNavbarMenu();
+
         }
+        private void CreateNavbarMenu()
+        {
+            HtmlGenericControl main = UList("MainMenu", "navbar");
+
+            main.Controls.Add(CreateMenuHTML("TechnologyName", "TechnologyURL"));
+            main.Controls.Add(CreateMenuParentHTML("TechnologyName2", "TechnologyURL2", "ProductDropDown2", "inventory"));
+
+            HtmlGenericControl ulDropDown = new HtmlGenericControl("ul");
+            ulDropDown.Attributes["id"] = "ProductDropDown2";
+            ulDropDown.Attributes["class"] = "dropdown-content collection";
+            //  foreach (DataRow r in dtDist.Rows)
+            {
+                ulDropDown.Controls.Add(CreateChildMenuHTML("drop", "drop"));
+                ulDropDown.Controls.Add(CreateChildMenuHTML("drop2", "drop2"));
+            }
+
+
+        
+            Panel1.Controls.Add(main);
+            Panel1.Controls.Add(ulDropDown);
+        }
+        
         private void FillLoginCard()
         {
             try
@@ -57,6 +82,38 @@ namespace ourShop
             Session.Abandon();
             Session.Clear();
             Response.Redirect("~/Home.aspx");
+        }
+
+        private HtmlGenericControl UList(string id, string cssClass)
+        {
+            HtmlGenericControl ul = new HtmlGenericControl("ul");
+            ul.ID = id;
+            ul.Attributes.Add("class", cssClass);
+            return ul;
+        }
+        private HtmlGenericControl CreateMenuHTML(string text, string url)
+        {
+            HtmlGenericControl li = new HtmlGenericControl("li");
+            li.InnerHtml = "<a href=" + string.Format("http://{0}", url) + ">" + text + "</a>";
+            return li;
+        }
+        private HtmlGenericControl CreateMenuParentHTML(string text,  string url, string dropDownId, string iconName)
+        {
+            //<li><a class="dropdown-trigger" href="#!" data-target="ProductDropDown">Products<i class="material-icons right">inventory arrow_drop_down</i></a></li>
+
+            HtmlGenericControl li = new HtmlGenericControl("li");
+            li.InnerHtml = "<a class='dropdown-trigger' href='#!' data-target='"+ dropDownId + "' href=" + string.Format("http://{0}", url) + ">" 
+                + text +
+               "<i class='material-icons right'>" + iconName + " arrow_drop_down</i></a>";
+            return li;
+        }
+        private HtmlGenericControl CreateChildMenuHTML(string text, string url)
+        {
+            //<li><a href="/Modules/AddProduct"><span>Add</span><i class="material-icons">add</i></a></li>
+
+            HtmlGenericControl li = new HtmlGenericControl("li");
+            li.InnerHtml = "<a href=" + string.Format("http://{0}", url) + ">" + text + "</a>";
+            return li;
         }
     }
 }
--- ourShop/Site.Master.designer.cs
+++ ourShop/Site.Master.designer.cs
@@ -33,6 +33,15 @@ namespace ourShop
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl mainPage;
 
         /// <summary>
+        /// Panel1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+
+        /// <summary>
         /// UnLoggedCard control.
         /// </summary>
         /// <remarks>
